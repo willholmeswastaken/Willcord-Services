402992a2657ef51a1af2faae0ac4f85374a5e32b
willholmeswastaken <willholmes97@icloud.com>
2021-07-08 09:48:49 +0100

    Added dynamo mappings on the properties

--- Willcord.Services/Servers/Willcord.Services.Servers.Models/Server.cs
+++ Willcord.Services/Servers/Willcord.Services.Servers.Models/Server.cs
@@ -1,11 +1,23 @@
+using EfficientDynamoDb.Attributes;
+
 namespace Willcord.Services.Servers.Models
 {
+    [DynamoDbTable("willcord-servers")]
     public class Server
     {
+        [DynamoDbProperty("Id", DynamoDbAttributeType.PartitionKey)]
         public string Id { get; set; }
 
+        [DynamoDbProperty("Name", DynamoDbAttributeType.SortKey)]
         public string Name { get; set; }
 
+        [DynamoDbProperty("CreatedById")]
         public string CreatedById { get; set; }
+
+        [DynamoDbProperty("DeletedOn")]
+        public string DeletedOn { get; set; }
+
+        [DynamoDbProperty("DeletedById")]
+        public string DeletedById { get; set; }
     }
 }
